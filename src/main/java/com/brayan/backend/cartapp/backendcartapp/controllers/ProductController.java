64a82e4349b7c5d4c76eee9cc3c7e4f751119f82
Brayan Sanchez <brayan_sanchez99@hotmail.es>
2023-09-29 18:17:22 -0600

    Establishing communication between frontend and backend

--- src/main/java/com/brayan/backend/cartapp/backendcartapp/controllers/ProductController.java
+++ src/main/java/com/brayan/backend/cartapp/backendcartapp/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.brayan.backend.cartapp.backendcartapp.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ import com.brayan.backend.cartapp.backendcartapp.models.entities.Product;
 import com.brayan.backend.cartapp.backendcartapp.services.ProductService;
 
 @RestController
-
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
 
     @Autowired
